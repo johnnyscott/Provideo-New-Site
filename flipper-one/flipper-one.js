8ee772f74edda9241fb29055a6b19b39fb300b9e
unknown <johnny@.(none)>
2010-09-17 05:02:17 -0500

    updates working on pager

--- flipper-one/flipper-one.js
+++ flipper-one/flipper-one.js
@@ -4,6 +4,8 @@
 /* initialize flipper one object */
 var flipperOne = {
 	iDefaultItemsPerPage   : 4,    // items per page defaults to 4
+  
+  iPagerImages : {onId: 'on', offId :'off', on:'page-on.gif',off:'page-off.gif',iWidth:8}, // images for pager
 	
 	iFadeOffset            : 70,  // fade in horizontal offset	(pixels)
 	iFadeInOffsetMillis    : 500,  // time offset for fade in 	
@@ -13,9 +15,13 @@ var flipperOne = {
 	iPageChangeDelayMillis : 8000, // delay between automatic page change
   
   iPageCount             : 1,    // number of pages (at least 1 even if empty)
-  iCurrentPage           : 1,    // current page defaults to 1 (not 0 indexed)
+  iCurrentPage           : -1,    // -1 indicates page startup (not 0 indexed)
 	
+  
+  /* returns reference to flipper and pager */
   o : function(){return $('#flipper-one');},
+  op : function() {return $('#pager');},
+  
   
 	/* animates height to given value */
 	setHeight : function(iHeight){
@@ -24,6 +30,33 @@ var flipperOne = {
       },'easein');
 	},
 
+  
+  /* calculates and returns total image width with margins */
+  pageImageWidth : function(){
+    // calculate total width of image with margins
+   return flipperOne.iPagerImages.iWidth + 
+            parseInt(flipperOne.op().find("img:eq(1)").css('margin-left').replace('px','')) +
+            parseInt(flipperOne.op().find("img:eq(1)").css('margin-right').replace('px',''))
+            parseInt(flipperOne.op().find("img:eq(1)").css('padding-left').replace('px','')) +
+            parseInt(flipperOne.op().find("img:eq(1)").css('padding-right').replace('px',''));  
+  },
+  
+  
+  /* returns offset to place pager on image from iCurrentPage => param iPage */
+  pageOnOffset : function(iPage){
+   var iWidth = flipperOne.pageImageWidth();
+   
+   if( flipperOne.iCurrentPage == -1){ // initial page set
+    return -(iWidth * flipperOne.iPageCount);
+   }else{ // all other page sets
+   }
+  },
+  
+  
+  /* shifts to page given */
+  toPage : function(iPage){
+   alert('to page ' + iPage);
+  },
   
   /* sets up flipper one */
 	flipperOne : function(){
@@ -38,7 +71,7 @@ var flipperOne = {
 		// Animate 1st page fade in
 		var iHeight = 0; 
 		for(var i=0; i<flipperOne.o().children().length;i++){	
-			// break after last item on page
+			// break after last item on 1st page
 			if(i > flipperOne.iDefaultItemsPerPage -1){ break; }else{/**/}
 			 
 			iHeight += flipperOne.o().find("div:eq("+i+")").height() +
@@ -52,15 +85,24 @@ var flipperOne = {
            left: "+="+flipperOne.iFadeOffset+"px",
            opacity: 1.00
           },'easein');
-						
+						 
 		}
 
 		// set height
 		flipperOne.setHeight(iHeight);
-
-    // setup images 
+		flipperOne.op().text('');    
+    flipperOne.op().css({'padding-left':    '14px'});
     
-		
+    // setup paging images 
+    for(var i = 0; i< flipperOne.iPageCount;i++){
+     flipperOne.op().append('<a href="#" onClick="javascript:flipperOne.toPage('+(i+1)+');">'+
+      '<img src="'+flipperOne.iPagerImages.off+'" id="'+flipperOne.iPagerImages.offId+i+'" /></a>');
+    }
+    flipperOne.op().append('<a href="#" style="cursor:default"><img style="position:relative;left:'+flipperOne.pageOnOffset(1)+'px" '+
+       'src="'+flipperOne.iPagerImages.on+'" id="'+flipperOne.iPagerImages.onId+'" /></a>');
+		flipperOne.iCurrentPage = 1;
+    
+    
 	}
 	  
 };
